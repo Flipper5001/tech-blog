1b93d275f1b5d55d62843b3fffba9f4b507fba40
Flipper <filipe@motionfirestudios.com.au>
2022-08-21 17:12:45 +0800

    added public folder with css and js files to affect front end and application operations

---
+++ public/js/login.js
@@ -0,0 +1,48 @@
+const loginForm = document.querySelector('.login-form');
+const signupForm = document.querySelector('.signup-form');
+
+async function login(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#user-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+async function signup(event){
+  event.preventDefault();
+  
+  const username = document.querySelector('#user-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+  
+  if (username && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+  
+loginForm.addEventListener('submit', login);
+signupForm.addEventListener('submit', signup);
+  
